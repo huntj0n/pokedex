8a89bb0d19c3db06ac6324cd41ad8fa78eb446f1
huntj0n <hunt.jon19@gmail.com>
2021-08-06 20:47:07 -0600

    server.js setup minus endpoints

---
+++ server/index.js
@@ -0,0 +1,41 @@
+//---PACKAGES---
+require('dotenv').config;
+const express = require('express');
+const massive = require('massive');
+
+//---CONTROLLERS---
+
+const { SERVER_PORT, CONNECTION_STRING } = procces.env;
+
+//---MIDDLEWARE---
+const app = express();
+
+app.use(express.json());
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then((db) => {
+    app.set('db', db)
+    console.log('Pokebase Connected')
+    app.listen(SERVER_PORT, () => console.log(`Catching them all on port: ${SERVER_PORT}`))
+}).catch((err) => {
+    console.log(err);
+    res.status(500).send(err)
+})
+
+//---ENDPOINTS---
+
+//team endpoints
+app.get()
+app.post()
+app.put()
+app.delete()
+
+//user endpoints
+app.get()
+app.post()
+app.put()
+app.delete()
